18591a27552b65e9919287630eb10cb97a12b317
kamelia918 <kameliaremaki@gmail.com>
2024-07-14 16:20:58 +0100

    html base template

---
+++ models/invigilator.js
@@ -0,0 +1,11 @@
+//models/user.js
+const mongoose = require('mongoose');
+
+const invgSchema = new mongoose.Schema({
+
+  
+})
+
+const Invigilator = mongoose.model('Invigilator', invgSchema);
+
+module.exports = Invigilator;
